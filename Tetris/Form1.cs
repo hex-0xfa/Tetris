8837ba0cf06070fde619cca8ea33f85ada4317f0
Chenxi Chu <cchu2@sdsu.edu>
2017-11-05 14:41:52 -0800

    My second Commit

--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -13,6 +13,7 @@ namespace Tetris
     public partial class Form1 : Form
     {
         private int m;
+        private int n;
         public Form1()
         {
             InitializeComponent();
